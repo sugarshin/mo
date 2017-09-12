5d64ecaaadeb00f61e8ee3cfc7a4f3ba9c6ab4b0
sugarshin <shinsugar@gmail.com>
2017-09-13 03:25:00 +0900

    Add favicon

--- components/Layout.js
+++ components/Layout.js
@@ -71,6 +71,7 @@ class Layout extends PureComponent {
           <meta charSet='utf-8' />
           <title>{`${title ? `${title} | ` : ''}${baseTitle}`}</title>
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+          <link rel='shortcut icon' href='/static/favicon.png' />
           <link
             rel='stylesheet'
             href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css'
