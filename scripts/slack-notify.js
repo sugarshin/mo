75a8eeb9aba05814f7c84abed0d8a8617f44a9d9
sugarshin <shinsugar@gmail.com>
2018-02-07 14:43:58 +0900

    Fix deploy

---
+++ scripts/slack-notify.js
@@ -0,0 +1,10 @@
+const { IncomingWebhook } = require('@slack/client')
+
+new IncomingWebhook(process.env.SLACK_WEBHOOK_URL)
+.send('`mo` has been updated :tada: <https://moci.now.sh/>', err => {
+  if (err) {
+    console.log('Error:\n\n', err)
+  } else {
+    console.log('Message sent successfully.')
+  }
+})
