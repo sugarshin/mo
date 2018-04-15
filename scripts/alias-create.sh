e03278682782a4bd1891ad27804e9d9b4cb81ec7
sugarshin <shinsugar@gmail.com>
2018-04-15 12:07:46 +0900

    Fix region

--- scripts/alias-create.sh
+++ scripts/alias-create.sh
@@ -3,4 +3,4 @@
 set -eu
 
 ./node_modules/.bin/now alias --token=$NOW_TOKEN && \
-./node_modules/.bin/now scale moci.now.sh all 1 --token=$NOW_TOKEN
+./node_modules/.bin/now scale moci.now.sh sfo 1 --token=$NOW_TOKEN
