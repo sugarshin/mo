a523f1029d9791b951e50ea1a92c57356f05c2c0
sugarshin <shinsugar@gmail.com>
2019-02-21 01:30:07 +0900

    wip [ci skip]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,8 @@
-import { Configuration } from 'webpack'
-import { Context } from '@nuxt/vue-app'
-
 export default {
   mode: 'universal',
   srcDir: 'src/',
   buildDir: 'build',
   dev: process.env.NODE_ENV !== 'production',
-
-  /*
-  ** Headers of the page
-  */
   head: {
     title: 'mo - CircleCI Client for Mobile Web',
     meta: [
@@ -19,55 +12,16 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-
-  /*
-  ** Customize the progress-bar color
-  */
   loading: { color: '#fff' },
-
-  /*
-  ** Global CSS
-  */
   css: [],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
   plugins: [],
-
-  /*
-  ** Nuxt.js modules
-  */
   modules: [
-    // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    // Doc: https://bootstrap-vue.js.org/docs/
     'bootstrap-vue/nuxt',
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/proxy',
   ],
-  /*
-  ** Axios module configuration
-  */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
+  proxy: {
+    '/api': { target: 'https://circleci.com' },
   },
-
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config: Configuration, ctx: Context) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module!.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-        })
-      }
-    }
-  }
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,32 +1,27 @@
 <template>
   <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        mo
-      </h1>
-      <h2 class="subtitle">
-        My grand Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <img :src="avatarUrl" />
+    <h1>{{ selectedEmail }}</h1>
   </section>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<script lang="ts">
+import Vue from 'vue';
+import '@nuxt/vue-app';
 
-export default {
-  components: {
-    Logo,
+export default Vue.extend({
+  async asyncData({ app }) {
+    const r = await app.$axios
+      .$get('/api/v1.1/me', {
+        params: { 'circle-token': '12543b07a533b9c7955a63879acae82e393fcc1c' },
+      })
+      .catch(() => ({}))
+    return { selectedEmail: r.selected_email, avatarUrl: r.avatar_url };
   },
-}
+})
 </script>
 
-<style>
+<style scoped>
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -34,25 +29,5 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
